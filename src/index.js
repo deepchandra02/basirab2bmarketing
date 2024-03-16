75b667ab5edf556490b02d747ffdfe16728ed671
Deep Chandra <deepc@qatar.cmu.edu>
2024-03-16 11:51:32 +0300

    fixed warnings

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import { ThemeProvider } from "@material-tailwind/react";
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -24,8 +23,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+initializeApp(firebaseConfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
