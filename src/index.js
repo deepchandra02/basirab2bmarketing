7c55b4f1079dfc2252c496b9085895759e0d5d3f
Deep Chandra <deepc@qatar.cmu.edu>
2024-03-16 11:33:45 +0300

    firebase setup complete

--- src/index.js
+++ src/index.js
@@ -5,6 +5,27 @@ import App from './App';
 
 import { ThemeProvider } from "@material-tailwind/react";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAv9K47JyDPPv5W4LjR-IsbGDxHIYfZ98g",
+  authDomain: "basirab2bmarketing.firebaseapp.com",
+  projectId: "basirab2bmarketing",
+  storageBucket: "basirab2bmarketing.appspot.com",
+  messagingSenderId: "689227499532",
+  appId: "1:689227499532:web:aa4255e4c28a3456870629",
+  measurementId: "G-F3QJH1E0W5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
