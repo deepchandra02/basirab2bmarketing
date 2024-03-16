629458214ccedc700cc4571fbf6f32c099360c14
Deep Chandra <deepc@qatar.cmu.edu>
2024-03-16 11:20:45 +0300

    initial setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Button } from "@material-tailwind/react";
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-red-500">
+        Hello world!
+      </h1>
+      <Button>Button</Button>
+    </>
+  );
+}
+
+export default App;
