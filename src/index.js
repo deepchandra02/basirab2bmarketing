a629b56ec9875ec29ccb398086be84cbb5142051
Deep Chandra <thedeepchandra@gmail.com>
2024-07-09 16:54:48 +0300

    security patch

--- src/index.js
+++ src/index.js
@@ -2,23 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-
 import { ThemeProvider } from "@material-tailwind/react";
-
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAv9K47JyDPPv5W4LjR-IsbGDxHIYfZ98g",
-  authDomain: "basirab2bmarketing.firebaseapp.com",
-  projectId: "basirab2bmarketing",
-  storageBucket: "basirab2bmarketing.appspot.com",
-  messagingSenderId: "689227499532",
-  appId: "1:689227499532:web:aa4255e4c28a3456870629",
-  measurementId: "G-F3QJH1E0W5",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
